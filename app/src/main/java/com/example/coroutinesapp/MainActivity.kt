5f94f751a9f71288d0a66343d89dd46632077919
daad1 <daadrasheid@gmail.com>
2021-12-10 21:53:52 +0300

    Advice

---
+++ app/src/main/java/com/example/coroutinesapp/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.coroutinesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.*
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.URL
+
+
+class MainActivity : AppCompatActivity() {
+
+    val adviceAPI = "https://api.adviceslip.com/advice"
+    private lateinit var textAdvice: TextView
+    private lateinit var buttonAdvice: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        textAdvice = findViewById<TextView>(R.id.tv_Show)
+        buttonAdvice = findViewById<Button>(R.id.btn_Advice)
+
+        buttonAdvice.setOnClickListener() {
+
+            requestAPI()
+        }
+
+    }
+
+    private fun requestAPI() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val data = async {
+
+                fetchRandomApi()
+            }.await()
+
+            if (data.isNotEmpty()) {
+                updateAdvice(data)
+            }
+        }
+    }
+
+    private fun fetchRandomApi(): String {
+        var response = ""
+
+        try {
+            response = URL(adviceAPI).readText(Charsets.UTF_8)
+
+
+        } catch (e: Exception) {
+
+            print("Error $e")
+        }
+        return response
+
+    }
+
+    private suspend fun updateAdvice(data: String) {
+        withContext(Dispatchers.Main) {
+
+            val JSONObject = JSONObject(data)
+            val slip = JSONObject.getJSONObject("slip")
+            val id = slip.getInt("id")
+            val advice = slip.getString("advice")
+
+            textAdvice.text = advice
+
+        }
+    }
+}
